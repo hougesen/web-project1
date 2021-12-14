693b323be29bd5451527ee053181ec346e0cdf26
Mads Hougesen <madshougesen@gmail.com>
2021-12-14 14:09:55 +0100

    refactor(calendar): converted to api endpoint instead

--- Controllers/RoutesApiController.cs
+++ Controllers/RoutesApiController.cs
@@ -4,10 +4,14 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AAOAdmin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace AAOAdmin.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/routes")]
     [ApiController]
     public class RoutesAPIController : ControllerBase
     {
@@ -18,14 +22,14 @@ namespace AAOAdmin.Controllers
             _context = context;
         }
 
-        // GET: api/RoutesAPI
+        // GET: api/routes
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Route>>> GetRoutes()
         {
             return await _context.Routes.ToListAsync();
         }
 
-        // GET: api/RoutesAPI/5
+        // GET: api/routes/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Route>> GetRoute(int id)
         {
@@ -40,7 +44,7 @@ namespace AAOAdmin.Controllers
         }
 
         // Updates userid in routes table with onclick and saves in database
-        // PUT: api/RoutesAPI/5
+        // PUT: api/routes/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}/{userId}")]
         public async Task<IActionResult> PutRoute(int id, int userId)
@@ -68,7 +72,7 @@ namespace AAOAdmin.Controllers
             return new JsonResult(route);
         }
 
-        // POST: api/RoutesAPI
+        // POST: api/routes
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<Route>> PostRoute(Route route)
@@ -79,7 +83,7 @@ namespace AAOAdmin.Controllers
             return CreatedAtAction("GetRoute", new { id = route.RouteId }, route);
         }
 
-        // DELETE: api/RoutesAPI/5
+        // DELETE: api/routes/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteRoute(int id)
         {
@@ -98,6 +102,32 @@ namespace AAOAdmin.Controllers
         private bool RouteExists(int id)
         {
             return _context.Routes.Any(e => e.RouteId == id);
+        }
+
+        // GET: api/routes/calendar
+        [HttpGet("calendar")]
+        public async Task<IActionResult> RouteCalendar()
+        {
+            DateTime currentDate = DateTime.Now;
+            DateTime firstDayOfMonth = new DateTime(currentDate.Year, currentDate.Month, 1);
+            DateTime lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddSeconds(-1);
+            var routes = await _context.Routes.Where(r => r.RouteStartDate >= firstDayOfMonth && r.RouteStartDate <= lastDayOfMonth).ToListAsync();
+            int daysInMonth = Int32.Parse(lastDayOfMonth.ToShortDateString().Substring(0, 2));
+            int[] calendar_dates = new int[daysInMonth];
+
+            foreach (Route route in routes)
+            {
+                if (route.RouteStartDate != null)
+                {
+                    int date_index = Int32.Parse(route.RouteStartDate.ToString().Substring(0, 2));
+                    if (date_index >= 0)
+                    {
+                        calendar_dates[date_index] += 1;
+                    }
+                }
+            }
+
+            return new JsonResult(calendar_dates);
         }
     }
 }
